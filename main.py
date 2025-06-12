9ce85177dad5bafb5f9b1ef5a5adef45e8308d95
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-06-12 08:32:16 +0200

    Set up basic game loop with quit functionality and set frame size

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import pygame
+import pygame
 
 from constants import (
     SCREEN_WIDTH,
@@ -11,9 +11,20 @@ from constants import (
 
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill("black")
+        pygame.display.flip()
+    pygame.quit()
 
 
 if __name__ == "__main__":
