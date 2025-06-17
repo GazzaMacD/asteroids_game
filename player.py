e635f65c0c3b1180bb14c89b1c20135e69a6a2b3
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-06-17 08:25:36 +0200

    Reduce number of shots using cool down timer

--- player.py
+++ player.py
@@ -2,7 +2,13 @@ import pygame
 import sys
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_COOLDOWN,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+)
 from shot import Shot
 
 
@@ -10,6 +16,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.countdown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +34,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.countdown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -51,5 +59,8 @@ class Player(CircleShape):
             sys.exit(0)
 
     def shoot(self):
+        if self.countdown > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.countdown = PLAYER_SHOOT_COOLDOWN
